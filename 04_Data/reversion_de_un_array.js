23a4400dd2dc58db8aec077ba74bc28d9b1a92a0
ango1994 <ango19941994@gmail.com>
2024-05-20 17:29:17 +0200

    Add reversion_de_un_array

---
+++ 04_Data/reversion_de_un_array.js
@@ -0,0 +1,19 @@
+function reverseArray(arr) {
+  const arrayToReturn = []
+  for (let i = arr.length - 1; i >= 0; i--) {
+    arrayToReturn.push(arr[i])
+  }
+  return arrayToReturn
+}
+
+function reverseArrayInPlace(arr) {
+  const middleArray = []
+  for (let i = array.length - 1; i >= 0; i--) {
+    const element = arr.pop()
+    middleArray.push(element)
+  }
+  arr.unshift(...middleArray)
+  return arr
+}
+
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]))
