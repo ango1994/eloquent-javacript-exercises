e9fab0cc4d99f4bbcecb7a712e69315b5c6ba56e
ango1994 <ango19941994@gmail.com>
2024-05-27 20:11:19 +0200

    Quitar la que he liao

--- 07_Robot_Project/robots/random_robot.js
+++
@@ -1,5 +0,0 @@
-import { roadGraph } from "../utils/build_graph.js"
-import { randomPick } from "../utils/random_pick.js"
-export function randomRobot(state) {
-  return { direction: randomPick(roadGraph[state.place]) }
-}
--- 07_Robot_Project/utils/random_pick.js
+++
@@ -1,4 +0,0 @@
-export function randomPick(array) {
-  let choice = Math.floor(Math.random() * array.length)
-  return array[choice]
-}
